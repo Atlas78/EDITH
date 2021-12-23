5d55c5e5ac1512b585fd3955286385aa41488cab
atlas78 <tanmaybairwa7470@gmail.com>
2021-12-23 11:27:03 +0530

    Import Modules

---
+++ EDITH.py
@@ -0,0 +1,13 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import wolframalpha
+import webbrowser
+import ctypes
+import psutil
+import pyautogui
+import os
+import sys
+import subprocess
+import random
