cfa6861e1fbd269bdf94ca508f8d91617d1131cc
atlas78 <tanmaybairwa7470@gmail.com>
2021-12-23 11:34:44 +0530

    Adding Chrome Path and Engine (pyttsx3)

--- EDITH.py
+++ EDITH.py
@@ -11,3 +11,10 @@ import os
 import sys
 import subprocess
 import random
+
+chrome_path ='C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+engine = pyttsx3.init('sapi5')
+client = wolframalpha.Client('E7Q874-J4279K73X4')
+voices = engine.getProperty('voices')
+print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
